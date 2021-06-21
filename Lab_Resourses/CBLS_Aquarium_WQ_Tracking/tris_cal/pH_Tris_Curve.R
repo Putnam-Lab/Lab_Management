a9aaa3e5cc472b1c2df9cb939da5d447082369f0
rosiebailey <rosalind_bailey@uri.edu>
2021-06-21 15:37:02 -0400

    Today's WQ

--- Lab_Resourses/CBLS_Aquarium_WQ_Tracking/tris_cal/pH_Tris_Curve.R
+++ Lab_Resourses/CBLS_Aquarium_WQ_Tracking/tris_cal/pH_Tris_Curve.R
@@ -1,6 +1,6 @@
 # modified for personal Lenovo PC at PT Whitney summer 2019
 # last modification on 20210621 by RB
-setwd("/Users/roxyr/Documents/GitHub/Lab_Management/WaterQuality/20210621.csv/") #set working directory
+setwd("/Users/roxyr/Documents/GitHub/Lab_Management/WaterQuality/tris_cal/") #set working directory
 
 Calib.Data <-read.table("20210621.csv", header=TRUE, sep=",", na.string="NA", as.is=TRUE) #reads in the data files
 model <-lm(mVTris ~ TTris, data=Calib.Data) #runs a linear regression of mV as a function of temperature
@@ -10,3 +10,4 @@ plot(mVTris ~ TTris, data=Calib.Data)
 abline(lm(mVTris ~ TTris, data=Calib.Data))
 legend('topleft', legend = bquote(R^2 == .(format(R2, digits = 3))), bty='n')
 # make sure to export the figure
+
