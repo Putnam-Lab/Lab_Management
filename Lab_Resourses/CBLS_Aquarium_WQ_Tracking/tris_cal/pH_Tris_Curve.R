f16b7579f3c7507d2f32ab329595f9556738cac3
rosiebailey <rosalind_bailey@uri.edu>
2021-06-24 16:01:00 -0400

    Water Quality

--- Lab_Resourses/CBLS_Aquarium_WQ_Tracking/tris_cal/pH_Tris_Curve.R
+++ Lab_Resourses/CBLS_Aquarium_WQ_Tracking/tris_cal/pH_Tris_Curve.R
@@ -2,7 +2,7 @@
 # last modification on 20210621 by RB
 setwd("/Users/roxyr/Documents/GitHub/Lab_Management/WaterQuality/tris_cal/") #set working directory
 
-Calib.Data <-read.table("20210621.csv", header=TRUE, sep=",", na.string="NA", as.is=TRUE) #reads in the data files
+Calib.Data <-read.table("20210623.csv", header=TRUE, sep=",", na.string="NA", as.is=TRUE) #reads in the data files
 model <-lm(mVTris ~ TTris, data=Calib.Data) #runs a linear regression of mV as a function of temperature
 coe <- coef(model) #extracts the coeffecients
 R2<-summary(model)$r.squared
